158d03c166bace442426b75601c7485231225e85
Pratik Hati <pratikhati.work@gmail.com>
2022-07-17 03:13:24 +0530

    17th July

---
+++ Seiveof Eratothenis.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    vector<int> primeRange(int M, int N)
+    {
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, M, K;
+        cin >> M >> N;
+        Solution ob;
+        vector<int> ans = ob.primeRange(M, N);
+        for (auto u : ans)
+            cout << u << " ";
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
